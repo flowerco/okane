e376abd4f0d1d6f624bd15972c4921d10b1aed21
Sam Flower <66141971+flowerco@users.noreply.github.com>
2022-10-11 20:05:54 +0100

    Fixed: sidebar menu formatting and subscriptions month

--- src/api/SubscriptionService.ts
+++ src/api/SubscriptionService.ts
@@ -1,8 +1,14 @@
 const rootUrl = 'http://localhost';
 
 export const getSubscriptions = async () => {
+  let output: any = '';
   return await fetch(
     `${rootUrl}:${process.env.REACT_APP_PORT}/subscriptions`, { credentials:"include" }
-  ).then((res) => res.json());
+  ).then((res) => {
+    const data = res.json();
+    output=data;
+    return data;
+  })
+  .finally(() => console.log('Final subs output: ', output));
   // .catch((err) => console.log('error @getSubscriptions', err));
 };
