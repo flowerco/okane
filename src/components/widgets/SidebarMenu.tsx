6c24a39df082845ec93496d1f1a060d1cfa29863
Simon Roope <simonroope@gmail.com>
2022-10-14 09:13:37 +0100

    added calendar - basics

--- src/components/widgets/SidebarMenu.tsx
+++ src/components/widgets/SidebarMenu.tsx
@@ -3,11 +3,14 @@ import '../../index.css';
 import { logout } from "../../redux/authSlice";
 import { removeJwtCookie } from "../../api/LoginService";
 import { toggleSidebar } from "../../redux/screenSlice";
+import { useNavigate } from 'react-router-dom';
+
 
 export const SidebarMenu = () => {
 
   const screenState = useAppSelector(state => state.screen);
   const dispatch = useAppDispatch();
+  const navigate = useNavigate();
 
   const handleLogout = () => {
     removeJwtCookie();
@@ -15,12 +18,17 @@ export const SidebarMenu = () => {
     dispatch(toggleSidebar())
   }
 
+  const handleRedirect = () => {
+    console.log('Redirect to: ')
+    navigate('/calendar');   
+  } 
+
   return (
     <>
       <div className={`menu-transition fixed z-20 h-[calc(100vh_-_4rem_-_5.3mm)] w-[100vw] text-white bg-[#428c97] ${screenState.sidebarOpen ? 'open' : 'closed'}`}>
         <ul className="h-full flex flex-col justify-between items-center text-3xl space-y-6 pt-6 pb-10">
           <div className="flex flex-col justify-center items-center space-y-6">
-            <li className="border-2 border-white rounded-lg py-3 px-8">Calendar</li>
+            <li className="border-2 border-white rounded-lg py-3 px-8" onClick={handleRedirect}>Calendar</li>
             <li className="border-2 border-white rounded-lg py-3 px-8">Categories</li>
             <li className="border-2 border-white rounded-lg py-3 px-8">Settings</li>
           </div>
