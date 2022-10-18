8b573ebbf470dc16e519f14762bee19889ee21a6
Simon Roope <simonroope@gmail.com>
2022-10-18 20:12:28 +0100

    Calendar Screen

--- src/components/screens/CalendarScreen.tsx
+++ src/components/screens/CalendarScreen.tsx
@@ -24,67 +24,28 @@ export const CalendarScreen = () =>{
   const status = calendarState.status;
   const calendarError = calendarState.error;
 
-  // Required data format
-  const data = [
-    {
-      id: 0,
-      title: 'Netfix: £11.99',
-      start: new Date(2022, 9, 3),
-      end: new Date(2022, 9, 3),
-      subscription: 'STR'
-    },
-    {
-      id: 1,
-      title: 'Amazon: £9.99',
-      start: new Date(2022, 9, 12),
-      end: new Date(2022, 9, 12),
-      subscription: 'STR'
-    },
-    {
-      id: 2,
-      title: 'Xow: £11.98',
-      start: new Date(2022, 9, 12),
-      end: new Date(2022, 9, 12),
-      subscription: 'STR'
+   
+  //  Data format from sequelize
+  //  { month_end_date: "2022-09-30T00:00:00.000Z", date: "2022-09-12T00:00:00.000Z", subscription_code: "GYM",
+  //    subscription_name: "Gym", merchant_name: "Fitness First", price: 21.99 }
+  //
+  //  Data format required by <Calendar/>
+  //  { id: 0, title: 'Netfix: £11.99', start: new Date(2022, 9, 3), end: new Date(2022, 9, 3), subscription: 'STR' }
+
+  const data = calendarState.transactions.map((e, i) => {
+
+    return {
+            id: i,
+            title: `${e.merchant_name}: £${e.price}`,
+            start: new Date(e.date),
+            end: new Date(e.date),
+            subscription: e.subscription_code,
     }
-  ]
-
-  // Sequelize data format
-  const sqlData = [
-    { 
-      month_end_date: "2022-01-31T00:00:00.000Z",
-      date: "2022-01-17T00:00:00.000Z",
-      subscription_code: "STR",
-      subscription_name: "Streaming",
-      merchant_name: "Netflix",
-      price: 10.99
-    },
-    {
-      month_end_date: "2022-09-30T00:00:00.000Z",
-      date: "2022-09-12T00:00:00.000Z",
-      subscription_code: "GYM",
-      subscription_name: "Gym",
-      merchant_name: "Fitness First",
-      price: 21.99
-    },
-    {
-      month_end_date: "2022-09-30T00:00:00.000Z",
-      date: "2022-09-15T00:00:00.000Z",
-      subscription_code: "GYM",
-      subscription_name: "Gym",
-      merchant_name: "Peleton",
-      price: 11.99
-    }]
-
+  })
 
   useEffect(() => {
     if (status === 'idle') {
       dispatch(fetchCalendar());
-    }
-    if (status === 'succeeded') {
-      // TODO Reformat data for <Calendar/> input
-      const latestMonth = Object.values(calendarState.transactions).sort((a,b) => new Date(b.month_end_date) - new Date(a.month_end_date))[0]  
-      const latestMonthsData = Object.values(calendarState.transactions).filter(e => e.month_end_date === latestMonth.month_end_date);
     }
   }, [status, dispatch]);
 
@@ -93,18 +54,17 @@ export const CalendarScreen = () =>{
   return (
     <div className='w-full h-full flex flex-col justify-center items-center'>
 
-      <h1 className='text-center text-2xl font-semibold text-white mb-4'>{dateMon}</h1>
-
       <div className="w-3/4 h-3/4">
         <Calendar
-          toolbar={false}
+          toolbar={true}
           views={'month'}
           events={data}
           startAccessor="start"
-          defaultDate={moment().toDate()}
+          defaultDate={dateMon}
           localizer={localizer}
           eventPropGetter={(data) => {
-            const backgroundColor = (data.subscription === 'STR') ? 'blue' : (data.subscription === 'GYM') ? 'green' : 'red';
+            const backgroundColor = (data.subscription === 'STR') ? 'blue' : (data.subscription === 'GYM') ? 'green' :
+                                         (data.subscription === 'CCD') ? 'red' : (data.subscription === 'MBP') ? 'orange' : 'purple';
               return { style: { backgroundColor } }
           }}
         />
