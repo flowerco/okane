d0d3c031e645151f65ccbd0327952226adec7070
Ben <ben.stimpson@outlook.com>
2022-10-19 18:23:23 +0100

    Fixed categories

--- src/components/screens/MainScreen.tsx
+++ src/components/screens/MainScreen.tsx
@@ -1,82 +1,82 @@
-import { useAppSelector } from "../../redux/hooks";
-import { SidebarMenu } from "../widgets/SidebarMenu";
-import { AnalysisScreen } from "./AnalysisScreen";
-import { LoginScreen } from "./LoginScreen";
-import { SummaryScreen } from "./SummaryScreen";
-import { CalendarScreen } from "./CalendarScreen";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import CategoriesScreen from "./CategoriesScreen";
-import { ContributorsScreen } from "./ContributorsScreen";
-import { YoutubeScreen } from "./YoutubeScreen";
-import { Loading } from "../widgets/Loading";
-import OpenBankingScreen from "./OpenBankingScreen";
-import { SearchList } from "../widgets/SearchList";
+import { useAppSelector } from '../../redux/hooks';
+import { SidebarMenu } from '../widgets/SidebarMenu';
+import { AnalysisScreen } from './AnalysisScreen';
+import { LoginScreen } from './LoginScreen';
+import { SummaryScreen } from './SummaryScreen';
+import { CalendarScreen } from './CalendarScreen';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import CategoriesScreen from './CategoriesScreen';
+import { ContributorsScreen } from './ContributorsScreen';
+import { YoutubeScreen } from './YoutubeScreen';
+import { Loading } from '../widgets/Loading';
+import OpenBankingScreen from './OpenBankingScreen';
+import { SearchList } from '../widgets/SearchList';
 
 export const MainScreen = ({
-  appLoading,
-  online,
+	appLoading,
+	online,
 }: {
-  appLoading: boolean;
-  online: boolean;
+	appLoading: boolean;
+	online: boolean;
 }) => {
-  const authState = useAppSelector((state) => state.authentication);
-  const { searchString } = useAppSelector((state) => state.screen);
+	const authState = useAppSelector((state) => state.authentication);
+	const { searchString } = useAppSelector((state) => state.screen);
 
-  if (appLoading) {
-    return (
-      <div className="h-[calc(100vh_-_4rem_-_5.3mm)] w-full flex justify-center items-center">
-        <Loading />
-      </div>
-    );
-  }
+	if (appLoading) {
+		return (
+			<div className='h-[calc(100vh_-_4rem_-_5.3mm)] w-full flex justify-center items-center'>
+				<Loading />
+			</div>
+		);
+	}
 
-  return (
-    <div
-      className={`relative w-full ${
-        online
-          ? "h-[calc(100vh_-_4rem_-_5.3mm)]"
-          : "h-[calc(100vh_-_8rem_-_5.3mm)]"
-      }`}
-    >
-      {searchString && <SearchList />}
-      {!online && (
-        <div className="flex items-center text-center h-16 bg-orange-500 py-3 px-6">
-          <p>Device is not online, some functionality may not be available.</p>
-        </div>
-      )}
-      <div className="h-full relative overflow-auto">
-        {authState.isAuthenticated ? (
-          <>
-            <div className="h-full relative">
-              <Routes>
-                <Route path="/connect" element={<OpenBankingScreen />}></Route>
-                <Route path="/" element={<SummaryScreen />}></Route>
-                <Route
-                  path="/analysis/:id"
-                  element={<AnalysisScreen />}
-                ></Route>
-                <Route path="/calendar" element={<CalendarScreen />}></Route>
-                <Route
-                  path="/categories"
-                  element={<CategoriesScreen />}
-                ></Route>
-                <Route path="/advice" element={<YoutubeScreen />}></Route>
-                <Route
-                  path="/contributors"
-                  element={<ContributorsScreen />}
-                ></Route>
-              </Routes>
-            </div>
-            <div className="absolute z-20 h-full top-0 left-0">
-              <SidebarMenu />
-            </div>
-          </>
-        ) : (
-          <>
-            <LoginScreen />
-          </>
-        )}
-      </div>
-    </div>
-  );
+	return (
+		<div
+			className={`relative w-full ${
+				online
+					? 'h-[calc(100vh_-_4rem_-_5.3mm)]'
+					: 'h-[calc(100vh_-_8rem_-_5.3mm)]'
+			}`}
+		>
+			{searchString && <SearchList />}
+			{!online && (
+				<div className='flex items-center text-center h-16 bg-orange-500 py-3 px-6'>
+					<p>Device is not online, some functionality may not be available.</p>
+				</div>
+			)}
+			<div className='h-full relative'>
+				{authState.isAuthenticated ? (
+					<>
+						<div className='h-full relative'>
+							<Routes>
+								<Route path='/connect' element={<OpenBankingScreen />}></Route>
+								<Route path='/' element={<SummaryScreen />}></Route>
+								<Route
+									path='/analysis/:id'
+									element={<AnalysisScreen />}
+								></Route>
+								<Route path='/calendar' element={<CalendarScreen />}></Route>
+								<Route
+									path='/categories'
+									element={<CategoriesScreen />}
+								></Route>
+								<Route path='/advice' element={<YoutubeScreen />}></Route>
+								<Route
+									path='/contributors'
+									element={<ContributorsScreen />}
+								></Route>
+							</Routes>
+						</div>
+						<div className='absolute z-20 h-full top-0 left-0'>
+							<SidebarMenu />
+						</div>
+					</>
+				) : (
+					<>
+						<LoginScreen />
+					</>
+				)}
+			</div>
+		</div>
+	);
 };
