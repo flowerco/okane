2c6cd8abd00ef331cf1092a9b6f75727085df084
Alex Kubbinga <63915427+AlexKubbinga@users.noreply.github.com>
2022-10-19 17:14:06 +0100

    fixed centering

--- src/components/screens/SummaryScreen.tsx
+++ src/components/screens/SummaryScreen.tsx
@@ -11,63 +11,63 @@ import { useNavigate } from 'react-router-dom';
 import { SubscriptionType } from '../../values/customTypes';
 
 export const SummaryScreen = () => {
-	const dispatch = useAppDispatch();
-	const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
 
-	const colors = PieChartColorList1;
-	const subscriptionsState = useAppSelector(selectSubs);
-	const subscriptions = subscriptionsState.data;
-	const status = subscriptionsState.status;
-	const error = subscriptionsState.error;
+  const colors = PieChartColorList1;
+  const subscriptionsState = useAppSelector(selectSubs);
+  const subscriptions = subscriptionsState.data;
+  const status = subscriptionsState.status;
+  const error = subscriptionsState.error;
 
-	useEffect(() => {
-		if (status === 'idle') {
-			dispatch(fetchSubs());
-		}
-	}, [dispatch, status]);
+  useEffect(() => {
+    if (status === 'idle') {
+      dispatch(fetchSubs());
+    }
+  }, [dispatch, status]);
 
-	// TODO: Loading/failed logic to be added to redux.
-	if (status === 'loading') {
-		return <Loading />;
-	}
-	if (status === 'failed') {
-		return <Error error={error} />;
-	}
+  // TODO: Loading/failed logic to be added to redux.
+  if (status === 'loading') {
+    return <Loading />;
+  }
+  if (status === 'failed') {
+    return <Error error={error} />;
+  }
 
-	const handleClick = async (item: SubscriptionType) => {
-		navigate(`analysis/${item.subscription_id}`);
-	};
+  const handleClick = async (item: SubscriptionType) => {
+    navigate(`analysis/${item.subscription_id}`);
+  };
 
-	const formatDate = new Date(subscriptionsState.month).toLocaleDateString(
-		'en-GB',
-		{
-			month: 'long',
-			year: '2-digit',
-		}
-	);
+  const formatDate = new Date(subscriptionsState.month).toLocaleDateString(
+    'en-GB',
+    {
+      month: 'long',
+      year: '2-digit',
+    }
+  );
 
-	return (
-		<div className='lg:flex lg:flex-row-reverse sm:grid sm:grid-cols-1 h-full w-full sm:justify-items-center'>
-			<div className='w-4/5 aspect-square relative'>
-				<MinPieChart data={subscriptions} colors={colors} />
-				<div className='text-green-400 text-5xl z-0 h-full w-full absolute top-0 left-0 align-center flex justify-center items-center'>
-					{`£${subscriptions
-						.reduce((accumulator, sub) => {
-							return accumulator + sub.monthlyPrice;
-						}, 0)
-						.toFixed(0)}`}
-				</div>
-			</div>
-			<div className='lg:flex-col  lg:w-3/5 lg:h-4/5 lg:m-28 text-center'>
-				<div className=' text-white text-3xl font-semibold'>{formatDate}</div>
-				<div className='lg:mt-20 flex flex-col w-full px-12 mt-4 pb-6'>
-					<ItemList
-						data={subscriptions}
-						colors={colors}
-						callback={handleClick}
-					/>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="lg:flex lg:flex-row-reverse sm:grid sm:grid-cols-1 h-full w-full justify-center items-center sm:justify-items-center">
+      <div className="flex w-4/5 aspect-square relative mx-auto">
+        <MinPieChart data={subscriptions} colors={colors} />
+        <div className="text-green-400 text-5xl z-0 h-full w-full absolute top-0 left-0 align-center flex justify-center items-center">
+          {`£${subscriptions
+            .reduce((accumulator, sub) => {
+              return accumulator + sub.monthlyPrice;
+            }, 0)
+            .toFixed(0)}`}
+        </div>
+      </div>
+      <div className="lg:flex-col  lg:w-3/5 lg:h-4/5 lg:m-28 text-center">
+        <div className=" text-white text-3xl font-semibold">{formatDate}</div>
+        <div className="lg:mt-20 flex flex-col w-full px-12 mt-4 pb-6">
+          <ItemList
+            data={subscriptions}
+            colors={colors}
+            callback={handleClick}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
